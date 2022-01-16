782861523b423a064c8aa7983979b163588d2f7e
WiiJoy MacOS <wiijoy@bk.ru>
2022-01-16 16:30:29 +0300

    refactor: cleanup div timeline code

--- custom-video/script.js
+++ custom-video/script.js
@@ -1,6 +1,5 @@
 let video = document.querySelector('.video__video'),
     fullTimeline = document.querySelector('.video__timeline'),
-    // currTimeline = document.querySelector('.timeline'),
     volume = document.querySelector('.video__volume__level'),
     currVolume = document.querySelector('.volume__line'),
     btnPlay = document.querySelector('.button__play'),
@@ -17,12 +16,6 @@ const playVideo = function() {
     btnPlayBig.classList.add('btn__hide')
     btnPause.classList.remove('btn__hide')
 
-    // videoPlay = setInterval(function() {
-    //     let currVideoTime = Math.round(video.currentTime);
-    //     let totalVideoTime = Math.round(video.duration);
-
-    //     currTimeline.style.width = (currVideoTime * 100) / totalVideoTime + '%';
-    // }, 10)
 }
 
 btnPlay.addEventListener('click', playVideo)
@@ -31,7 +24,6 @@ btnPlayBig.addEventListener('click', playVideo)
 
 btnPause.addEventListener('click', function() {
     video.pause();
-    // clearInterval(videoPlay);
 
     btnPlay.classList.remove('btn__hide')
     btnPlayBig.classList.remove('btn__hide')
@@ -44,5 +36,7 @@ function timeline () {
 }
 
 video.addEventListener('timeupdate', timeline);
+
+
 
 
