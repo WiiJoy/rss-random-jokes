d21306b413ab55f078a785c051ce5cf2c323e54c
WiiJoy MacOS <wiijoy@bk.ru>
2022-01-19 11:58:09 +0300

    refactor: playVideo function to play-pause

--- custom-video/script.js
+++ custom-video/script.js
@@ -1,36 +1,31 @@
 let video = document.querySelector('.video__video'),
     fullTimeline = document.querySelector('.video__timeline'),
     volume = document.querySelector('.video__volume__level'),
-    // currVolume = document.querySelector('.volume__line'),
     btnPlay = document.querySelector('.button__play'),
-    btnPause = document.querySelector('.button__pause'),
     btnPlayBig = document.querySelector('.video__play'),
     btnVolume = document.querySelector('.button__volume'),
     poster = document.querySelector('.video__poster');
 
-const playVideo = function() {
-    poster.classList.add('btn__hide')
-    video.play();
-
-    btnPlay.classList.add('btn__hide')
-    btnPlayBig.classList.add('btn__hide')
-    btnPause.classList.remove('btn__hide')
-
-}
-
 videoVolume ();
 
+const playVideo = function() {
+
+    if (btnPlay.classList.contains('button__pause')) {
+        video.pause();
+
+        btnPlay.classList.remove('button__pause')
+        btnPlayBig.classList.remove('btn__hide')
+    } else {
+        poster.classList.add('btn__hide')
+        video.play();
+
+        btnPlay.classList.add('button__pause')
+        btnPlayBig.classList.add('btn__hide')
+    }
+}
+
 btnPlay.addEventListener('click', playVideo)
-
 btnPlayBig.addEventListener('click', playVideo)
-
-btnPause.addEventListener('click', function() {
-    video.pause();
-
-    btnPlay.classList.remove('btn__hide')
-    btnPlayBig.classList.remove('btn__hide')
-    btnPause.classList.add('btn__hide')
-})
 
 function timeline () {
     let currTime = (Math.floor(video.currentTime) / (Math.floor(video.duration) / 100));
