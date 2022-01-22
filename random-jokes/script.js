23520cf2b1ff11df071cc6db106bc3404f817c03
Vitaliy <wiification@gmail.com>
2022-01-22 18:57:28 +0300

    feat: random background with update quote

--- random-jokes/script.js
+++ random-jokes/script.js
@@ -1,16 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
     const quoteText = document.querySelector('.quote__text'),
           quoteAuthor = document.querySelector('.quote__author'),
-          btn = document.querySelector('.button');
+          btn = document.querySelector('.button'),
+          main = document.querySelector('.main');
     
-    getQuote(quoteText, quoteAuthor);
+    getQuote(quoteText, quoteAuthor, main);
+    main.style.background = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
 
     btn.addEventListener('click', function() {
         getQuote(quoteText, quoteAuthor);
+        main.style.background = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
     });
 });
 
-async function getQuote(text, author) {
+async function getQuote(text, author, main) {
     const url = 'https://favqs.com/api/qotd';
     const res = await fetch(url);
     const data = await res.json();
