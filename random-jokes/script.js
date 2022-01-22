dbcebf33b2b07df2d605ddb0d99ed9e46f700fef
Vitaliy <wiification@gmail.com>
2022-01-22 18:42:15 +0300

    feat: update quote after button click

--- random-jokes/script.js
+++ random-jokes/script.js
@@ -1,27 +1,38 @@
-document.addEventListener('DOMContentLoaded', async function() {
+document.addEventListener('DOMContentLoaded', () => {
     const quoteText = document.querySelector('.quote__text'),
-          quoteAuthor = document.querySelector('.quote__author');
+          quoteAuthor = document.querySelector('.quote__author'),
+          btn = document.querySelector('.button');
     
-    let res = await getQuote();
+    getQuote(quoteText, quoteAuthor);
 
-    let quote = {
-        text: res.text,
-        author: res.author
-    };
+    btn.addEventListener('click', function() {
+        getQuote(quoteText, quoteAuthor);
+    });
 
-    console.log(quote);
+    // let quote = {
+    //     text: res.text,
+    //     author: res.author
+    // };
 
-    quoteText.innerHTML = quote.text;
-    quoteAuthor.innerHTML = quote.author;
+    // console.log(quote);
+
+    // quoteText.innerHTML = quote.text;
+    // quoteAuthor.innerHTML = quote.author;
+
+
+
 });
 
-async function getQuote() {
+async function getQuote(text, author) {
     const url = 'https://favqs.com/api/qotd';
     const res = await fetch(url);
     const data = await res.json();
     console.log(data);
     console.log(data.quote.body, data.quote.author);
 
-    return { text: data.quote.body, author: data.quote.author };
+    text.innerHTML = data.quote.body;
+    author.innerHTML = data.quote.author;
+
+    // return { text: data.quote.body, author: data.quote.author };
 }
 
