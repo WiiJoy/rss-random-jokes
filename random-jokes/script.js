a3f6c89156b34bff3561c124a085c0bae29208d2
Vitaliy <wiification@gmail.com>
2022-01-22 18:30:21 +0300

    feat: handling get answer to DOM

--- random-jokes/script.js
+++ random-jokes/script.js
@@ -1,5 +1,18 @@
-document.addEventListener('DOMContentLoaded', () => {
-    getQuote();
+document.addEventListener('DOMContentLoaded', async function() {
+    const quoteText = document.querySelector('.quote__text'),
+          quoteAuthor = document.querySelector('.quote__author');
+    
+    let res = await getQuote();
+
+    let quote = {
+        text: res.text,
+        author: res.author
+    };
+
+    console.log(quote);
+
+    quoteText.innerHTML = quote.text;
+    quoteAuthor.innerHTML = quote.author;
 });
 
 async function getQuote() {
@@ -7,5 +20,8 @@ async function getQuote() {
     const res = await fetch(url);
     const data = await res.json();
     console.log(data);
+    console.log(data.quote.body, data.quote.author);
+
+    return { text: data.quote.body, author: data.quote.author };
 }
 
