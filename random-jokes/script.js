b1b8aa97103d639cd176dadb5e8baf6f29f0fee9
Vitaliy <wiification@gmail.com>
2022-01-22 19:44:09 +0300

    feat: add images with unsplash

--- random-jokes/script.js
+++ random-jokes/script.js
@@ -2,25 +2,38 @@ document.addEventListener('DOMContentLoaded', () => {
     const quoteText = document.querySelector('.quote__text'),
           quoteAuthor = document.querySelector('.quote__author'),
           btn = document.querySelector('.button'),
-          main = document.querySelector('.main');
+          main = document.querySelector('.main'),
+          img = document.querySelector('.quote__image');
     
-    getQuote(quoteText, quoteAuthor, main);
+    getQuote(quoteText, quoteAuthor, img);
     main.style.background = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
 
     btn.addEventListener('click', function() {
-        getQuote(quoteText, quoteAuthor);
+        getQuote(quoteText, quoteAuthor, img);
         main.style.background = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
     });
 });
 
-async function getQuote(text, author, main) {
-    const url = 'https://favqs.com/api/qotd';
-    const res = await fetch(url);
+async function getQuote(text, author, img) {
+    const res = await fetch('https://favqs.com/api/qotd');
     const data = await res.json();
     console.log(data);
     console.log(data.quote.body, data.quote.author);
 
     text.innerHTML = data.quote.body;
     author.innerHTML = data.quote.author;
+
+    getImage(author.innerHTML, img);
 }
+
+async function getImage(author, img) {
+    const key = 'DSAe9oxwGIf74LcAJdtRp7K_rURwb-189PCdIE_9r4M';
+    const urlReq = `https://api.unsplash.com/photos/random?orientation=landscape&query=${author}&client_id=${key}`;
+    const res = await fetch(urlReq);
+    const data = await res.json();
+    console.log(data);
+
+    img.src = data.urls.regular;
+}
+
 
