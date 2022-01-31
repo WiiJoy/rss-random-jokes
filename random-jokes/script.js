aab599d3443573e272fb7f912e4c977e83e9f98a
WiiJoy MacOS <wiijoy@bk.ru>
2022-01-31 08:17:20 +0300

    refactor: remove change background color function

--- random-jokes/script.js
+++ random-jokes/script.js
@@ -2,26 +2,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const quoteText = document.querySelector('.quote__text'),
           quoteAuthor = document.querySelector('.quote__author'),
           btn = document.querySelector('.button'),
-          body = document.querySelector('body'),
           blur = document.querySelector('.blur');
     
-    let color = randomColor();
-
     getQuote(quoteText, quoteAuthor, blur);
-    body.style.background = color;
 
     btn.addEventListener('click', function() {
-        color = randomColor();
-        getQuote(quoteText, quoteAuthor, container);
-        body.style.background = color;
+        getQuote(quoteText, quoteAuthor, blur);
     });
 });
 
 async function getQuote(text, author, img) {
     const res = await fetch('https://favqs.com/api/qotd');
     const data = await res.json();
-    console.log(data);
-    console.log(data.quote.body, data.quote.author);
 
     text.innerHTML = data.quote.body;
     author.innerHTML = data.quote.author;
@@ -34,13 +26,6 @@ async function getImage(author, img) {
     const urlReq = `https://api.unsplash.com/photos/random?orientation=landscape&query=${author}&client_id=${key}`;
     const res = await fetch(urlReq);
     const data = await res.json();
-    console.log(data);
 
-    // img.src = data.urls.regular;
-
-    img.style.backgroundImage = `url(${data.urls.regular})`;
-}
-
-function randomColor() {
-    return `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+    img.style.backgroundImage = `url(${data.urls.small})`;
 }
