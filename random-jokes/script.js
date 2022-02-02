2d3853b1a67faa0d2d9fdcc4c497c4b304d6e59e
WiiJoy MacOS <wiijoy@bk.ru>
2022-02-02 09:32:57 +0300

    refactor: cleanup

--- random-jokes/script.js
+++ random-jokes/script.js
@@ -33,12 +33,9 @@ async function getQuote(text, author, img, lang) {
     text.style.opacity = 0
     author.style.opacity = 0
     if (lang === 'en') {
-        console.log('en lang')
         res = await fetch('https://favqs.com/api/qotd');
         data = await res.json();
         
-        // text.innerHTML = data.quote.body;
-        // author.innerHTML = data.quote.author;
         setTimeout(() => {
             text.innerHTML = data.quote.body;
             author.innerHTML = data.quote.author;
@@ -46,7 +43,6 @@ async function getQuote(text, author, img, lang) {
             author.style.opacity = 1
         }, 300)
     } else {
-        console.log('ru lang')
         res = await fetch('./assets/json/quotes.json');
         let dataObj = await res.json()
         data = dataObj[Math.floor(Math.random() * dataObj.length)];
@@ -57,25 +53,8 @@ async function getQuote(text, author, img, lang) {
             author.style.opacity = 1
         }, 300)
     }
-
     
-    console.log(data)
     img.style.opacity = 0
     img.style.backgroundImage = `url('./assets/img/${Math.round(Math.random() * 19 + 1)}.jpeg')`;
     img.style.opacity = 1
-    
-    // setTimeout(() => {
-        
-    // }, 500)
-
-    // getImage(img);
-}
-
-async function getImage(img) {
-    const key = 'DSAe9oxwGIf74LcAJdtRp7K_rURwb-189PCdIE_9r4M';
-    const urlReq = `https://api.unsplash.com/photos/random?orientation=landscape&query=nature&client_id=${key}`;
-    const res = await fetch(urlReq);
-    const data = await res.json();
-
-    img.style.backgroundImage = `url(${data.urls.small})`;
 }
