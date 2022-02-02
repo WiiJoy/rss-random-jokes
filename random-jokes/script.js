13e14bf605d9e5fc641b5ce0e2edb0d648ae10cf
WiiJoy MacOS <wiijoy@bk.ru>
2022-02-02 07:47:07 +0300

    feat: changes quote lang

--- random-jokes/script.js
+++ random-jokes/script.js
@@ -2,21 +2,52 @@ document.addEventListener('DOMContentLoaded', () => {
     const quoteText = document.querySelector('.quote__text'),
           quoteAuthor = document.querySelector('.quote__author'),
           btn = document.querySelector('.button'),
-          blur = document.querySelector('.blur');
+          blur = document.querySelector('.blur'),
+          langTools = document.querySelector('.langs'),
+          langItem = document.querySelectorAll('.lang');
     
-    getQuote(quoteText, quoteAuthor, blur);
+    let langQuotes = 'en'
+
+    getQuote(quoteText, quoteAuthor, blur, langQuotes);
 
     btn.addEventListener('click', function() {
-        getQuote(quoteText, quoteAuthor, blur);
+        getQuote(quoteText, quoteAuthor, blur, langQuotes);
     });
+
+    langTools.addEventListener('click', (ev) => {
+        if (ev.target.dataset.lang) {
+            langItem.forEach(item => item.classList.remove('lang_active'))
+            langQuotes = ev.target.dataset.lang
+            ev.target.classList.add('lang_active')
+    
+            getQuote(quoteText, quoteAuthor, blur, langQuotes);
+        }
+    })
 });
 
-async function getQuote(text, author, img) {
-    const res = await fetch('https://favqs.com/api/qotd');
-    const data = await res.json();
+async function getQuote(text, author, img, lang) {
 
-    text.innerHTML = data.quote.body;
-    author.innerHTML = data.quote.author;
+    let res = null
+    let data = null
+
+    if (lang === 'en') {
+        console.log('en lang')
+        res = await fetch('https://favqs.com/api/qotd');
+        data = await res.json();
+        text.innerHTML = data.quote.body;
+        author.innerHTML = data.quote.author;
+    } else {
+        console.log('ru lang')
+        res = await fetch('./assets/json/quotes.json');
+        let dataObj = await res.json()
+        data = dataObj[Math.floor(Math.random() * dataObj.length)];
+        text.innerHTML = data.text;
+        author.innerHTML = data.author;
+    }
+
+    
+    console.log(data)
+    
 
     getImage(img);
 }
