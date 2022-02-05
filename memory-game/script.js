816436df9f0291076a6cb802f91b74ba828824d0
WiiJoy MacOS <wiijoy@bk.ru>
2022-02-05 19:43:44 +0300

    feat: add lock to cards in check logic

--- memory-game/script.js
+++ memory-game/script.js
@@ -5,16 +5,17 @@ document.addEventListener('DOMContentLoaded', () => {
     let firstCard = '',
         secondCard = '',
         steps = 0,
-        handledCards = 0;
+        handledCards = 0,
+        lock = false;
 
     cards.addEventListener('click', (ev) => {
         if (ev.target.classList.contains('card__item') && !ev.target.parentNode.classList.contains('card_upend')) cardUpend(ev.target.parentNode);
     })
 
     function cardUpend(card) {
-            // card.parentNode.classList.remove('card_upend');
-            // card.parentNode.querySelector('.card__back').style.opacity = 1
-            // card.parentNode.querySelector('.card__front').style.opacity = 0
+
+        if (lock) return
+
         const front = card.querySelector('.card__front'),
               back = card.querySelector('.card__back');
 
@@ -26,6 +27,7 @@ document.addEventListener('DOMContentLoaded', () => {
             firstCard = card
         } else {
             secondCard = card
+            lock = true
         }
 
         if (firstCard && secondCard) {
@@ -39,12 +41,14 @@ document.addEventListener('DOMContentLoaded', () => {
             handledCards += 2
             firstCard = ''
             secondCard = ''
+            lock = false
         } else {
             setTimeout(() => {
                 removeCard(firstCard)
                 removeCard(secondCard)
                 firstCard = ''
                 secondCard = ''
+                lock = false
             }, 400)
             console.log('removes: ', firstCard, secondCard)
             
