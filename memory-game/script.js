9e836f0b3a56a04b04167a2a6fc84c30fe4669bf
WiiJoy MacOS <wiijoy@bk.ru>
2022-02-05 18:37:46 +0300

    feat: open front with upend

--- memory-game/script.js
+++ memory-game/script.js
@@ -11,5 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
 function cardUpend(card) {
         console.log('upend')
         card.parentNode.classList.toggle('card_upend');
+        card.parentNode.querySelector('.card__back').style.opacity = 0
+        card.parentNode.querySelector('.card__front').style.opacity = 1
     }
 
