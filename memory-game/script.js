a6259e32e78263ee1bee70c7b9a763486fd658fe
WiiJoy MacOS <wiijoy@bk.ru>
2022-02-05 18:48:27 +0300

    fix: fix second click to card

--- memory-game/script.js
+++ memory-game/script.js
@@ -2,25 +2,21 @@ document.addEventListener('DOMContentLoaded', () => {
     const cards = document.querySelector('.cards');
 
     cards.addEventListener('click', (ev) => {
-        if (ev.target.classList.contains('card__item')) cardUpend(ev.target);
+        if (ev.target.classList.contains('card__item') && !ev.target.parentNode.classList.contains('card_upend')) cardUpend(ev.target);
     })
 
     
 })
 
 function cardUpend(card) {
-        console.log('upend')
-
-        if (card.parentNode.classList.contains('card_upend')) {
-            card.parentNode.classList.remove('card_upend');
-            card.parentNode.querySelector('.card__back').style.opacity = 1
-            card.parentNode.querySelector('.card__front').style.opacity = 0
-        } else {
+        // if (card.parentNode.classList.contains('card_upend')) {
+            // card.parentNode.classList.remove('card_upend');
+            // card.parentNode.querySelector('.card__back').style.opacity = 1
+            // card.parentNode.querySelector('.card__front').style.opacity = 0
+        // } else {
             card.parentNode.classList.add('card_upend');
             card.parentNode.querySelector('.card__back').style.opacity = 0
             card.parentNode.querySelector('.card__front').style.opacity = 1
-        }
-
-        
+        // }
     }
 
