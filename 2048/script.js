ef46a25b4db3a8e38cd66e183ce9498a69db9095
WiiJoy MacOS <wiijoy@bk.ru>
2022-02-18 21:44:28 +0300

    feat: add test left move

--- 2048/script.js
+++ 2048/script.js
@@ -40,5 +40,66 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    function handleGameProcess() {
+        for (let i = 0; i < 4; i++){
+			for (let j = 0; j < 4; j++){
+				let currCell = document.querySelector(`#c${i}${j}`);
+				if (gameData[i][j] === 0) {
+					currCell.innerHTML = '';
+					currCell.className = 'game__cell';
+				} else {
+					currCell.innerHTML = gameData[i][j];
+					currCell.className = `game__cell game__cell_${+gameData[i][j]}`;
+				}
+			}
+		}
+    }
+
+    function handleLeftMove() {
+        let startCondition = '' + gameData
+
+        for (let i = 0; i < 4; i++) {
+            handleLeftMoveRow(i)
+        }
+
+        let finalCondition = '' + gameData
+
+        if (startCondition !== finalCondition) {
+            getRandomCell()
+            handleGameProcess()
+        }
+
+
+    }
+
+    function handleLeftMoveRow(row) {
+        for (let cell = 0; cell < 3; cell++){	
+			var nextCell = getNextElementInRow(row, cell);
+			if (nextCell !== -1) {
+				if (gameData[row][cell] === 0) {
+					gameData[row][cell] = gameData[row][nextCell];
+					gameData[row][nextCell] = 0;
+					cell--;
+				} else if (gameData[row][cell] === gameData[row][nextCell]) {
+					gameData[row][cell] *= 2;
+					gameData[row][nextCell] = 0;
+				}
+			} else {
+				break;
+			}
+		}
+    }
+
+    function getNextElementInRow(row, cell) {
+        for (let i = cell + 1; i < 4; i++){
+			if (this.mydata[row][i] !== 0) {
+				return i;
+			}
+		}
+		return -1;
+    }
+
+    
+
 
 })
